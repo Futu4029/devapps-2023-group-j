c221950e3619693901d29003586515a96a61ccc1
Es mi nombre <notAMail@example.com>
2023-03-22 21:08:02 -0300

    subimos la base del proyecto

---
+++ src/main/java/ar/edu/unq/devapps/grupoJ202301/backenddevappsapt/BackendDevappsAptApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.devapps.grupoJ202301.backenddevappsapt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDevappsAptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDevappsAptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/devapps/grupoJ202301/backenddevappsapt/BackendDevappsAptApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.devapps.grupoJ202301.backenddevappsapt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevappsAptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
