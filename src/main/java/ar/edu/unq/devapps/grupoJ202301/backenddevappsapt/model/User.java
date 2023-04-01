3265a351305352af492ee09ef0d8075e5003fbaf
Juan Pablo Contardo <contardo.juanpablo@gmail.com>
2023-04-01 00:30:50 -0300

    Subimos los cambios en la clase con las anotations

--- src/main/java/ar/edu/unq/devapps/grupoJ202301/backenddevappsapt/model/User.java
+++ src/main/java/ar/edu/unq/devapps/grupoJ202301/backenddevappsapt/model/User.java
@@ -1,6 +1,8 @@
 package ar.edu.unq.devapps.grupoJ202301.backenddevappsapt.model;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -10,14 +12,40 @@ import lombok.RequiredArgsConstructor;
 @Table(name="users")
 public class User {
 
-    @Id
-    private Long id;
+    @Column(nullable = false)
+    @Size(min=3, max=30)
     private String name;
+
+
+    @Column(nullable = false)
+    @Size(min=3, max=30)
     private String surname;
+
+
+
+    @Column(nullable = false)
+    @Email
     private String email;
+
+
+    @Column(nullable = false)
+    @Size(min=10, max=30)
     private String address;
+
+    @Column(nullable = false)
+    @Pattern(regexp="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*().,<>{}[\\]<>?_=+\\-|;:\\'\\\"\\/]])(?!.*\\s).{8,20}$")
+    @Size(min=6, max=32)
     private String password;
+
+
+    @Column(nullable = false)
+    @Size(min=22, max=22)
     private String cvu;
+
+
+    @Id
+    @SequenceGenerator(name = "walletSeq", sequenceName = "walletSeq", initialValue = 00000001, allocationSize = 1)
+    @GeneratedValue(generator = "walletSeq")
     private String walletAddress;
 
 
