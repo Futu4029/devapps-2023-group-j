252591716001440f1309f94a50a781fb2bcad697
Futu4029 <nofuture.gonza@gmail.com>
2023-04-18 23:44:55 -0300

    adding details on swagger

--- src/main/java/ar/edu/unq/devapps/grupoj202301/backenddevappsapt/BackendDevappsAptApplication.java
+++ src/main/java/ar/edu/unq/devapps/grupoj202301/backenddevappsapt/BackendDevappsAptApplication.java
@@ -1,9 +1,12 @@
 package ar.edu.unq.devapps.grupoj202301.backenddevappsapt;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@OpenAPIDefinition(info = @Info(title = "Crypto API", version = "1.0", description = "First approach"))
 public class BackendDevappsAptApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendDevappsAptApplication.class, args);
