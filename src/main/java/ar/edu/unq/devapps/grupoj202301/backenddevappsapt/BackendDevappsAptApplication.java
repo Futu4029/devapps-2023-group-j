598ef7257916683be7b442a89d74eeeb0cbee5d6
Futu4029 <nofuture.gonza@gmail.com>
2023-06-24 11:46:30 -0300

    Scheduler to get quotation every 10 minutes

--- src/main/java/ar/edu/unq/devapps/grupoj202301/backenddevappsapt/BackendDevappsAptApplication.java
+++ src/main/java/ar/edu/unq/devapps/grupoj202301/backenddevappsapt/BackendDevappsAptApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Crypto API", version = "1.0", description = "First approach"))
 @EnableAspectJAutoProxy
+@EnableScheduling
 @ComponentScan("ar.edu.unq.devapps.grupoj202301.backenddevappsapt")
 public class BackendDevappsAptApplication {
 	public static void main(String[] args) {
