c1e63e65032c568e9c2b9e27a66f802fdddf0570
Futu4029 <nofuture.gonza@gmail.com>
2023-06-27 19:52:25 -0300

    retry

--- src/main/java/ar/edu/unq/devapps/grupoj202301/backenddevappsapt/security/JwtTokenProvider.java
+++ src/main/java/ar/edu/unq/devapps/grupoj202301/backenddevappsapt/security/JwtTokenProvider.java
@@ -3,24 +3,29 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import java.util.Date;
 
 @Component
+@PropertySource(value={"classpath:application.properties"})
 public class JwtTokenProvider {
 
     private final String secretKey;
+    private final Long exp_date;
 
 
-    public JwtTokenProvider(@Value("${app.security.jwt.secret-key}") String secretKey){
+    public JwtTokenProvider(@Value("${app.security.jwt.secret-key}") String secretKey,
+                            @Value("${app.security.expiration-time}") Long exp_date){
         this.secretKey = secretKey;
+        this.exp_date = exp_date;
     }
 
     public String generateToken(Authentication authentication){
         String email = authentication.getName();
         Date now = new Date();
-        Date expirationToken = new Date(now.getTime() + 300000L);
+        Date expirationToken = new Date(now.getTime() + exp_date);
 
         String token = Jwts.builder()
                 .setSubject(email)
